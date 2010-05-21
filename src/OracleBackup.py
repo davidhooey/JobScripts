e0509cf3417fea346845f0a2dbde1ec90f5d7bb6
David Hooey <davidhooey@gmail.com>
2010-05-21 16:26:15 -0400

    Initial logging started

--- src/OracleBackup.py
+++ src/OracleBackup.py
@@ -7,6 +7,9 @@ import smtplib
 class RunCommand():
         
     def __init__(self):
+        self.f = open("/home/oracle/Scripts/OracleBackup.log",'a')
+        
+        self.logger("Initializing", "Started")
         
         # Get the current time and set the fileprefix.
         now = datetime.datetime.now() 
@@ -52,20 +55,30 @@ class RunCommand():
         self.receivers = ['davidh@opentext.com']
         self.emailheader =  "From: David Hooey <davidh@opentext.com>\n"
         self.emailheader += "To: David Hooey <davidh@opentext.com>\n"
-        self.emailheader += "Subject: OracleBackup: ogre\n\n"        
+        self.emailheader += "Subject: OracleBackup: ogre\n\n"
+        
+        self.logger("Initializing", "Finished")
+        
+    def logger(self,section,msg):
+        self.f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S,") + section + "," + msg)
         
     def run(self):
+        self.logger("Running", "Started")
         self.status['expdpcmd'] = os.system(self.expdpcmd)
         self.status['movecmd'] = os.system(self.movecmd)
         self.status['compresscmd'] = os.system(self.compresscmd)
         self.status['removecmd'] = os.system(self.removecmd)
+        self.logger("Running", "Finished")
             
     def rsyncFiles(self):
         # mount -t cifs //panzer.opentext.net/ogre -o username=ogre,password=Oracle12$
         # rsync -avz --no-o --no-g --no-p --no-t /home/oracle/Backups/ /mnt/panzer/`date +%Y-%m`
+        self.logger("Synchronizing", "Started")        
         self.status['rsynccmd'] = os.system(self.rsynccmd)
+        self.logger("Synchronizing", "Finished")
 
     def removeOldBackups(self):
+        self.logger("BackupCleanup", "Started")
         # Remove local backup files which are 7 days old or older.
         for file in os.listdir(self.backupsdir):
             filetime = os.path.getmtime(self.backupsdir + file) 
@@ -73,18 +86,21 @@ class RunCommand():
             fileage = currenttime - filetime
             if fileage > 604800:
                 os.unlink(self.backupsdir + file)
-                self.status['removeOldBackups'].append(str(self.backupsdir + file))  
+                self.status['removeOldBackups'].append(str(self.backupsdir + file))
+        self.logger("BackupCleanup", "Finished")  
 
     def sendStatusEmail(self):
+        self.logger("SendingEmail", "Started")
         self.message = self.emailheader + "Oracle Data Pump Backup: " + self.timestr + "\n"
         
         self.message += "\nCommand Status:\n"
         for s in self.status:
-            self.message += "\t" + s + ": "
-            if self.status[s] == 0:
-                self.message += "Success\n"
-            else:
-                self.message += "Failed\n"
+            if s != 'removeOldBackups':
+                self.message += "\t" + s + ": "
+                if self.status[s] == 0:
+                    self.message += "Success\n"
+                else:
+                    self.message += "Failed\n"
         
         self.message += "\nOld Backup Removal:\n"
         for f in self.status['removeOldBackups']:
@@ -95,6 +111,8 @@ class RunCommand():
             smtpObj.sendmail(self.sender, self.receivers, self.message)
         except SMTPException:
             print("Error: unable to send email")
+            
+        self.logger("SendingEmail", "Finished" + str(self.status))
 
 rc = RunCommand()
 rc.run()
